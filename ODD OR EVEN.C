1d6cf220afc165441bb54e63acdd896e0d002db9
SENTHIL KUMAR.B <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-25 20:11:16 +0530

    Add files via upload

---
+++ ODD OR EVEN.C
@@ -0,0 +1,13 @@
+//PROGRAM TO CHECK ODD OR EVEN 
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value for n: ");
+    scanf("%d", &n);
+    if(n%2==0)
+        printf("%d is even.",n);
+    else
+        printf("%d is odd.",n);  
+    return 0;
+}
