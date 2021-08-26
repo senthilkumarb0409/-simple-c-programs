c2c57185cd786051d8ed0a71b56f537e2e0ca16c
SENTHIL KUMAR.B <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-26 21:38:50 +0530

    Add files via upload

---
+++ SQUARE AND CUBE OF NO.C
@@ -0,0 +1,12 @@
+//SQUARE AND CUBE OF A NUMBER
+#include<stdio.h>
+int main()
+{
+	int num ,square,cube;
+	printf("Enter the value: ");
+	scanf("%d",&num);
+	square=num*num;
+	printf("\nsquare of the given number  is %d",square);
+	cube=num*num*num;
+	printf("\ncube of the given number is %d ",cube);
+}
