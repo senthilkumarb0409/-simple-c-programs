bf198c9bc035113ab8bea16eda1c9c64fa11ddc9
SENTHIL KUMAR.B <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-27 21:11:30 +0530

    Add files via upload

---
+++ MATRIX(SUM).C
@@ -0,0 +1,40 @@
+//SUM OF MATRIX PROGRAM
+#include<stdio.h>
+ 
+int main()
+{
+    int mat[10][10];
+    int i,j,row,col,sum=0;
+    printf("Enter the order of matrix:\n");
+    scanf("%d%d",&row,&col);
+    printf("Enter the elements of the matrix\n");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+ 
+    printf("The matrix\n");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            if(i==j)
+            {
+                sum=sum+mat[i][j];
+            }
+        }
+    }
+ 
+    printf("The sum of diagonal elements is = %d\n",sum);
+}
